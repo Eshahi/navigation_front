7abc1e950c46ae1dbdabe3dab5f87e492bdb4e2b
eshahi <yousefisoroush1@gmail.com>
2024-01-14 04:05:44 +0330

    floors initial test

---
+++ src/adminPanel/AdminPanel.jsx
@@ -0,0 +1,15 @@
+// AdminPanel.jsx
+
+import React from 'react';
+import FloorList from '../components/floorComponents/FloorList';
+
+const AdminPanel = () => {
+    return (
+        <div>
+            <h1>Admin Panel</h1>
+            <FloorList />
+        </div>
+    );
+};
+
+export default AdminPanel;
