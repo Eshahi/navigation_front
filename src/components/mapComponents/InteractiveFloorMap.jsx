7325276355771e24bf30370111e6dfc1f1f225ce
eshahi <yousefisoroush1@gmail.com>
2024-01-17 17:49:01 +0330

    admin panel completed

--- src/components/mapComponents/InteractiveFloorMap.jsx
+++ src/components/mapComponents/InteractiveFloorMap.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { Stage, Layer, Rect, Text, Group } from "react-konva";
 
 const InteractiveFloorMap = ({ rooms, onRoomSelect, onMapClick }) => {
-
     return (
         <Stage width={window.innerWidth} height={window.innerHeight} onClick={onMapClick}>
             <Layer>
-
                 {rooms.map((room) => (
-
                     <Group
                         key={room.id}
+                        x={room.x}
+                        y={room.y}
                         draggable
                         onDragEnd={(e) => {
+                            // Call onRoomSelect with the updated room position
                             onRoomSelect({
                                 ...room,
                                 x: e.target.x(),
@@ -20,27 +20,19 @@ const InteractiveFloorMap = ({ rooms, onRoomSelect, onMapClick }) => {
                             });
                         }}
                     >
-
                         <Rect
-                            x={room.x}
-                            y={room.y}
                             width={room.width}
                             height={room.height}
                             fill="#64b5f6"
                             stroke="#0d47a1"
                             strokeWidth={1}
                         />
-
                         <Text
                             text={room.name}
-                            x={room.x}
-                            y={room.y}
                             fontSize={15}
                             fill="#fff"
                         />
-
                     </Group>
-
                 ))}
             </Layer>
         </Stage>
